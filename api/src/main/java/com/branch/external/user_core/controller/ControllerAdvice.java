3b189328ec6e0b5bf98f169f52ac5bb769589cde
Ted Erickson <ted.erickson@comcast.net>
2024-10-15 08:08:39 -0600

    Use ErrorMessage

--- api/src/main/java/com/branch/external/user_core/controller/ControllerAdvice.java
+++ api/src/main/java/com/branch/external/user_core/controller/ControllerAdvice.java
@@ -3,6 +3,7 @@ package com.branch.external.user_core.controller;
 import com.branch.external.user_core.exception.InvalidRequestException;
 import com.branch.external.user_core.exception.NotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.springdoc.api.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,15 +15,15 @@ public class ControllerAdvice {
 
     @ExceptionHandler(InvalidRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleBadRequest(final InvalidRequestException exception) {
+    public ErrorMessage handleBadRequest(final InvalidRequestException exception) {
         log.error("handleBadRequest {}", exception.getMessage());
-        return exception.getMessage();
+        return new ErrorMessage(exception.getMessage());
     }
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String handleNotFound(final NotFoundException exception) {
+    public ErrorMessage handleNotFound(final NotFoundException exception) {
         log.info("handleNotFound {}", exception.getMessage());
-        return exception.getMessage();
+        return new ErrorMessage(exception.getMessage());
     }
 }
